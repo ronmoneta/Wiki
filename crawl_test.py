13a65ebe5567a458bb8f7622c360d13b9c340fd8
ronmoneta <30723810+ronmoneta@users.noreply.github.com>
2017-08-19 15:26:45 +0300

    Loop
    Fixed the loop for JS_crawler. doesnt add all the relevent urls

--- crawl_test.py
+++ crawl_test.py
@@ -12,11 +12,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from PIL import Image
 
 URL = "http://www.zionistarchives.org.il/"
+JS_URL = "http://www.zionistarchives.org.il/Pages/AdvancedSearch.aspx?ts=archive"
 PARSER = 'html.parser'
 PIC_LOCAL_LINK = r'C:\Users\user\Documents\Crawler\{0}.jpg'
 DEBUG_FILE = r"C:\Users\user\Documents\Crawler\names.txt"
@@ -31,12 +33,9 @@ HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 def crawler(url):
 	urls = [url]
 	visited = [url]
-	link_list = []
 	imgs = []
 	while len(urls) > 0:
 		try:
-			#browser,results = parse_js(urls[0])
-			#htmltext = browser.page_source
 			htmltext = connect(urls[0])
 			soup = BeautifulSoup(htmltext, PARSER)
 			print urls[0]
@@ -46,17 +45,63 @@ def crawler(url):
 				if url in tag['href'] and tag['href'] not in visited:
 					urls.append(tag['href'])
 					visited.append(tag['href'])
-			#for link in results.find_elements_by_class_name('gsa-click-link'):
-				#link_list.append(link.get_attribute("href"))
-			#urls = urls + link_list
 			urls = list(set(urls))
-			imgs = imgs + get_pics(soup)
+			imgs = imgs + get_pics(soup, url)
 		except:
 			print "nooop!!!!!!"
 			urls.pop(0)
 	imgs = list(set(imgs))
 	img_handler(imgs,url)
-	#browser.quit()
+
+def js_crawler(url):
+	urls = [url]
+	visited = [url]
+	imgs = []
+	browser = ''
+	browser = parse_js(urls[0])
+	select = Select(browser.find_element_by_class_name('SelectedType'))
+	#while len(urls) > 0:
+		#try:
+	index = 1
+	for i in xrange(len(select.options) - 1):
+		select.select_by_index(index)
+		wait = WebDriverWait(browser, 5)
+		try:
+			element = WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.NAME, "to_year")))
+			element.send_keys("1946")
+		except:
+			pass
+		confirm = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "ShowBtnCenter")))
+		confirm.click()
+		htmltext = browser.page_source
+		soup = BeautifulSoup(htmltext, PARSER)
+		for tag in soup.findAll('a',href=True):
+			tag['href'] = urlparse.urljoin(url,tag['href'])
+			if url in tag['href'] and tag['href'] not in visited:
+				urls.append(tag['href'])
+				visited.append(tag['href'])
+		for tag in soup.findAll('tr',itemlink=True):
+			tag['itemlink'] = urlparse.urljoin(url,tag['itemlink'])
+			if tag['itemlink'] not in visited:
+				holder = tag['itemlink']
+				if '&amp' in holder:
+					holder = holder.replace('&amp', '&')
+				urls.append(holder)
+				visited.append(holder)	
+		print 'Finished {0} round!'.format(index)
+		index += 1
+		#browser.get(url)
+		select = Select(browser.find_element_by_class_name('SelectedType'))
+	urls = urls
+	urls = list(set(urls))
+	imgs = imgs + get_pics(soup, url)
+	#except:
+		#print "Url's broken!!!!!!"
+		#urls.pop(0)
+	imgs = list(set(imgs))
+	img_handler(imgs,url)
+	if browser != '':
+		browser.quit()
 
 def img_handler(imgs,url):
 	#Recives list of imgs and the relevant url. try to download the imgs and save them locally.
@@ -110,12 +155,13 @@ def connect(url):
 	htmltext = res.read()
 	return htmltext
 
-def get_pics(soup):
+def get_pics(soup, url):
 	#Recives souped text, finds all the imgs in it.
 	imgs = []
 	for tag in soup.findAll('img',src=True):
 		img_link = tag['src']
-		imgs.append(img_link) 
+		if url not in img_link:
+			imgs.append(url + img_link) 
 	imgs = list(set(imgs))
 	return imgs
 
@@ -150,14 +196,15 @@ def parse_js(url):
 	#Recives webpage. parses it and returns it as an HTML.
 	browser = webdriver.Chrome()
 	browser.get(url)
-	wait = WebDriverWait(browser, 60)
-	results = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'search-result')))
-	return browser, results
+	return browser
 
 def main():
-	crawler(URL)
+	#crawler(URL)
+	js_crawler(JS_URL)
 
 if __name__ == "__main__":
 	main()
 
 #For the State Archive: Why does the JS returns only the first article/link? make JS part and HTML part work together.
+#TODO: Make it run on all pages (right now will add to urls only screen displayed pages (1-6), but no more). 
+#NOTE: Some categories dont have to_year filter
